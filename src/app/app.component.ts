4770afb1a977ebab76170bb5eb0791bb43adf420
saket-kr-jha <65586198+saket-kr-jha@users.noreply.github.com>
2024-06-22 22:42:05 +0530

    1st way to dynamically load component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {
   Component,
+  ComponentFactoryResolver,
   Inject,
   OnInit,
   TemplateRef,
@@ -19,6 +20,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { concatAll } from 'rxjs';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @Component({
   selector: 'app-root',
@@ -29,7 +31,7 @@ import { concatAll } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   showPrimary: boolean = true;
-  constructor(private _fb: FormBuilder) {}
+  constructor(private _fb: FormBuilder, ) {}
 
   response: any;
   dynamicForm = this._fb.group({});
@@ -39,58 +41,70 @@ export class AppComponent implements OnInit {
   @ViewChild('placeholder', { read: ViewContainerRef, static: true })
   public container!: ViewContainerRef;
 
-  @ViewChild('primary', { static: true })
-  public primary!: TemplateRef<any>;
-
-  @ViewChild('secondary', { static: true })
-  public secondary!: TemplateRef<any>;
-
-  ngOnInit(): void {
-    let primaryView: ViewRef, secondaryView: ViewRef;
-    setInterval(() => {
-      this.container.detach();
-      if (this.showPrimary) {
-        if (primaryView) {
-          this.container.insert(primaryView);
-        } else {
-          this.container.createEmbeddedView(this.primary, { text: 'primary' });
-        }
-      } else {
-        if (secondaryView) {
-          this.container.insert(secondaryView);
-        } else {
-          this.container.createEmbeddedView(this.secondary, {
-            text: 'secondary',
-          });
-        }
-      }
-      this.showPrimary = !this.showPrimary;
-    },1000);
-    this.getFormData();
+  ngOnInit() {
+    // 1st way to dynamically load component
+    // this.container.clear();
+    // const component = this.container.createComponent(DynamicComponent);
+    // component.instance.description = "Dynamic component";
+    // component.instance.clicked.subscribe(()=> {
+    //   console.log("clicked");
+    // })
   }
 
-  getFormData() {
-    this.formsData.getFormFields().subscribe((data: any) => {
-      this.response = data.data;
-      this.setFormsData(this.response);
-      console.log(this.response);
-    });
-  }
+  // @ViewChild('primary', { static: true })
+  // public primary!: TemplateRef<any>;
 
-  setFormsData(controls: any) {
-    for (const control of controls) {
-      const validators = [];
-      if (control.validators.required) {
-        validators.push(Validators.required);
-      }
-      this.dynamicForm.addControl(
-        control.name,
-        this._fb.control(control.value, validators)
-      );
-    }
-  }
+  // @ViewChild('secondary', { static: true })
+  // public secondary!: TemplateRef<any>;
 
-  saveForm() {
-    console.log(this.dynamicForm.value);
-  }
+  // ngOnInit(): void {
+  //   let primaryView: ViewRef, secondaryView: ViewRef;
+  //   setInterval(() => {
+  //     this.container.detach();
+  //     if (this.showPrimary) {
+  //       if (primaryView) {
+  //         this.container.insert(primaryView);
+  //       } else {
+  //         this.container.createEmbeddedView(this.primary, { text: 'primary' });
+  //       }
+  //     } else {
+  //       if (secondaryView) {
+  //         this.container.insert(secondaryView);
+  //       } else {
+  //         this.container.createEmbeddedView(this.secondary, {
+  //           text: 'secondary',
+  //         });
+  //       }
+  //     }
+  //     this.showPrimary = !this.showPrimary;
+  //   },1000);
+  //   this.getFormData();
+  // }
+
+  // getFormData() {
+  //   this.formsData.getFormFields().subscribe((data: any) => {
+  //     this.response = data.data;
+  //     this.setFormsData(this.response);
+  //     console.log(this.response);
+  //   });
+  // }
+
+  // setFormsData(controls: any) {
+  //   for (const control of controls) {
+  //     const validators = [];
+  //     if (control.validators.required) {
+  //       validators.push(Validators.required);
+  //     }
+  //     this.dynamicForm.addControl(
+  //       control.name,
+  //       this._fb.control(control.value, validators)
+  //     );
+  //   }
+  // }
+
+  // saveForm() {
+  //   console.log(this.dynamicForm.value);
+  // }
+
+ 
 }
--- src/app/dynamic/dynamic.component.ts
+++ src/app/dynamic/dynamic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dynamic',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './dynamic.component.html',
   styleUrl: './dynamic.component.css'
 })
-export class DynamicComponent {
+export class DynamicComponent implements OnInit {
+
+  @Input() public description : string = "";
+  @Output() public clicked = new EventEmitter();
+
+  constructor( ){}
+
+  ngOnInit(): void {
+    console.log("Inside dynamic oninit");
+  }
+  
+  handleClick(){
+    this.clicked.emit();
+  }
 
 }
